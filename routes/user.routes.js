659672a2649de8a19a936b8a3def08fedb923fab
TejasJuniwal13 <tejasjuniwal13@gmail.com>
2025-06-13 19:49:41 +0530

    Profile Route Created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator')
+const {authUserMiddleware} = require('../middlewares/auth.middleware');
+
 
 const { userController } = require('../controllers/user.controller');
 
 
-
+//register Route
 router.post('/register',
     body('email').isEmail().withMessage('Email address should be valid'),
     body('password').isLength({ min: 4 }).withMessage('password must be 4 characters long'),
     userController.createUserController);
 
+
+//login Route
 router.post('/login', 
     body('email').isEmail().withMessage('Email address should be valid'),
     body('password').isLength({min:4}).withMessage('Password must be 4 characters long'),
 
     userController.loginController
 )
+
+
+//Profile Route
+router.get('/profile', authUserMiddleware.authUser , userController.profileController);
 
 
 
